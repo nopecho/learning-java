6cc78cfb2f4ff709f3f03531bc32a0fc07134eca
nopecho <92011565+nopecho@users.noreply.github.com>
2021-10-12 16:55:19 +0900

    Update MyInfoApp.java

--- 1012/OpenCahllenge1/src/MyInfoApp.java
+++ 1012/OpenCahllenge1/src/MyInfoApp.java
@@ -2,9 +2,9 @@
 public class MyInfoApp {
 
 	public static void main(String[] args) {
-		String name = "조현준";
+		String name = "HyunJun";
 		int old = 26;
-		String Class ="빅데이터";
+		String Class ="BigData";
 		
 		System.out.print("name:"+name+"\nold:"+old+"\nClass:"+Class+"");
 	}
