9571bac5b0516ff939b01a76bcaacb2ac995674f
nopecho <92011565+nopecho@users.noreply.github.com>
2021-10-29 23:43:20 +0900

    Add files via upload

---
+++ 1027/Quiz_12.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Quiz_12 {
+
+	public static void main(String[] args) {
+		String[] words= {"television","computer","mouse","phone"};
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i=0;i<words.length;i++) {
+			char[] q = words[i].toCharArray();
+			
+				for(int j=0;j<q.length;j++) {	
+				int random = (int)(Math.random()*q.length);
+				char temp2;
+				temp2=q[i];
+				q[i]=q[random];
+				q[random]=temp2;
+			}
+				
+			System.out.printf("Q%d. %s의 정답을 입력하세요.>>",i+1,new String (q));
+			String a=sc.nextLine();
+			
+			if(words[i].equals(a.trim())) {
+				System.out.printf("맞았습니당.%n%n");
+			}else {
+				System.out.printf("틀렸습니당.%n%n");
+			}
+			
+		}sc.close();
+	}
+
+}
