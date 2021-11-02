8b4b84a629a14712eca039280ce3dd46e21d23e4
nopecho <92011565+nopecho@users.noreply.github.com>
2021-11-02 19:58:39 +0900

    Add files via upload

---
+++ 1102/Quiz_5_6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Circle{
+	private double x,y;
+	private int radius;
+	
+	public Circle(double x,double y,int radius) {
+		this.x=x;
+		this.y=y;
+		this.radius=radius;
+	}
+	int getRadius() {
+		return this.radius;	
+	}
+	public void show() {
+		System.out.println("가장 면적이 큰 원은 ("+x+","+y+") "+radius);
+	}
+}
+public class Quiz_5_6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Circle[] c = new Circle[3];
+		for(int i=0;i<c.length;i++) {
+			System.out.print("x, y, readius >>");
+			double x = sc.nextDouble();
+			double y = sc.nextDouble();
+			int radius= sc.nextInt();
+			c[i]=new Circle(x,y,radius);
+		}
+		int max = 0;
+		for (int i=0;i<c.length;i++) {
+			if( c[i].getRadius()>max ) {
+				max=c[i].getRadius();				
+			}
+		}
+		
+		sc.close();
+	}
+}
