c684f1f708ecc9e1dc5958f33fb7eeb395e37ceb
nopecho <92011565+nopecho@users.noreply.github.com>
2021-11-15 15:58:57 +0900

    Update Quiz_9_StackApp.java

--- 1111/Quiz_9_StackApp.java
+++ 1111/Quiz_9_StackApp.java
@@ -1,42 +1,46 @@
 import java.util.Scanner;
-interface Stack{ //스택 인터페이스
+interface Stack{
 	int length();
 	int capacity();
 	String pop();
 	boolean push(String val);
 }
 
-class StringStack implements Stack{ //인터페이스를 상속받은 StringStack 클래스 (해당 클래스는 인터페이스의 모든 메소드 오버라이드 해야됨)
+class StringStack implements Stack{ //interface를 상속받은 class는 interface의 모든 메소드를 구현해야함(오버라이딩)
 	Scanner sc = new Scanner(System.in);
-	public String[] s;
+	public String[] s; //스택이 들어갈 공간 할당
 	StringStack(int length){
 		this.s = new String[length];
 	}
 	
 	@Override
-	public int length() {
+	public int length() { //저장한 공간
 		int count=0;
 		for(int i=0;i<s.length;i++) {
 			if(s[i]!=null) count++;
 		}
 		return count;
 	}
+	
 	@Override
-	public int capacity() {
+	public int capacity() { //저장 가능한 공간
 		return s.length-length();
 	}
 	
 	@Override
-	public String pop() { //현재 스택의 0번째 문자열 리턴 메소드( 해당 메소드 실행하면 0번째 문자열이 리턴되고 1번 문자열이 0번 문자열이 됨)
-		if(capacity()==0) {
+	public String pop() { // stack의 0번째 출력
+		String temp;
+		if(length()==0) {
 			return null;
-		}return s[0];
+		}temp=s[capacity()];
+		s[capacity()]=null;
+		return temp;
 	}
 	
 	@Override
-	public boolean push(String val) { //현재 스택에 문자열 추가 메소드(추가 된 문자열은 스택의 0번째에 추가)
+	public boolean push(String val) { // 파라미터로 들어온 String타입 밸류를 stack의 0번째에 추가(추가 될때마다 0번째에 추가,기존 0번째는 1번째로 한칸씩 밀려남)
 		if(capacity()==0) {
-			System.out.println("[!] 스택이 가득 차서 푸시 불가");
+			System.out.println("스택이 가득차서 푸쉬 불가");
 			return false;
 		}s[capacity()-1]=val;
 		return true;
@@ -49,14 +53,13 @@ public class Quiz_9_StackApp {
 		System.out.print("총 스택 저장 공간의 크기 입력 : ");
 		int length = sc.nextInt();
 		StringStack app = new StringStack(length);	
-		
 		while(true) {
 			System.out.print("문자열 입력 : ");
 			String str = sc.next();
 			if(str.equals("그만")) {
 				System.out.print("스택에 저장된 모둔 문자열 팝 : ");
 				for(int i=0;i<length;i++) {
-					System.out.print(app.s[i]+" ");
+					System.out.print(app.pop()+" ");
 				}
 				break;
 			}
