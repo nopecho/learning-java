8c4a58fb9f0783bef15cfc60c3da86f2e1ffc869
nopecho <92011565+nopecho@users.noreply.github.com>
2021-11-27 01:43:36 +0900

    파라미터 수정

--- MMS-intelliJ/src/util/MemberElementList.java
+++ MMS-intelliJ/src/util/MemberElementList.java
@@ -4,10 +4,10 @@ public class MemberElementList {
     private final String[] element = {"아이디", "이름", "나이", "전화번호", "주소", "이메일", "취미"};
     private final String[] newElement;
 
-    MemberElementList(String string) {
+    MemberElementList() {
         newElement = new String[element.length];
         for (int i = 0; i < newElement.length; i++) {
-            newElement[i] = string + element[i] + " : ";
+            newElement[i] = "new 회원 "+ element[i] + " : ";
         }
     }
 
