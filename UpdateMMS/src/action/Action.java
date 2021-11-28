13494e5b1513b048445021112fec177ff7b72874
nopecho <92011565+nopecho@users.noreply.github.com>
2021-11-28 17:45:52 +0900

    Delete UpdateMMS/src directory

--- UpdateMMS/src/action/Action.java
+++
@@ -1,8 +0,0 @@
-package action;
-
-import util.MemberDB;
-import java.util.Scanner;
-
-public interface Action {
-	void execute(Scanner sc, MemberDB DB) throws Exception;
-}
--- UpdateMMS/src/svc/MemberListService.java
+++
@@ -1,9 +0,0 @@
-package svc;
-
-import util.MemberDB;
-
-public class MemberListService {
-    public boolean isEmptyMember(MemberDB DB) {
-        return !DB.getDB().isEmpty();
-    }
-}
