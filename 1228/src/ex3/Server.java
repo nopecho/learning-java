bfc5122f2556280c939f7d58f432941d12f9f58d
nopecho <ch.jooon@gmail.com>
2021-12-28 17:33:11 +0900

    추가 : 소켓 스레드 클래스

--- 1228/src/ex3/Server.java
+++ 1228/src/ex3/Server.java
@@ -8,28 +8,81 @@ public class Server {
     public static void main(String[] args) {
         ServerSocket serverSocket;
         Socket socket = null;
-        BufferedReader in = null;
         try {
             serverSocket = new ServerSocket(9999);
             System.out.println("연결 준비중..");
             socket = serverSocket.accept();
-            System.out.println(socket.getInetAddress()+" 클라이언트 접속\n연결 성공");
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+            System.out.println(socket.getInetAddress() + " 클라이언트 접속\n연결 성공");
+            Thread th = new Thread(new socketRunnable(socket));
+            th.start();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } finally {
             try {
-                if(socket != null) socket.close();
-            } catch (IOException e){
+                if (socket != null) socket.close();
+            } catch (IOException e) {
                 System.out.println("연결 오류");
             }
         }
     }
 
-    public static String clac(String input){
-        StringTokenizer st = new StringTokenizer(input," ");
-        if(st.countTokens() != 3) return "입력 오류";
-        return null;
+    public static String clac(String input) {
+        StringTokenizer st = new StringTokenizer(input, " ");
+        String result = "";
+
+        if (st.countTokens() != 3) result = "입력 오류";
+
+        int op1 = Integer.parseInt(st.nextToken());
+        String del = st.nextToken();
+        int op2 = Integer.parseInt(st.nextToken());
+
+        switch (del) {
+            case "+":
+                result = Integer.toString(op1 + op2);
+                break;
+            case "-":
+                result = Integer.toString(op1 - op2);
+                break;
+            case "*":
+                result = Integer.toString(op1 * op2);
+                break;
+            default:
+                result = "입력 오류";
+        }
+        return result;
+    }
+}
+
+class socketRunnable implements Runnable {
+    private final Socket socket;
+
+    socketRunnable(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            while (true) {
+                String inputMsg = in.readLine();
+                if (inputMsg.equalsIgnoreCase("bye")) {
+                    System.out.println(socket.getInetAddress() + " <연결 종료>");
+                    break;
+                }
+                String result = Server.clac(inputMsg);
+                System.out.println(inputMsg+" = "+result+"\n");
+                out.write(result);
+                out.flush();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                if(socket != null) socket.close();
+            }catch (IOException e){
+                System.out.println("연결 오류 !");
+            }
+        }
     }
 }
