73c50aeb029c04cffbdf52d6b06b2d6eec66e460
nopecho <ch.jooon@gmail.com>
2021-12-28 14:22:56 +0900

    추가 : 서버프로그래밍 수식계산클라이언트 클래스

--- 1228/src/client/CalcClientEx.java
+++ 1228/src/client/CalcClientEx.java
@@ -5,7 +5,38 @@ import java.net.*;
 import java.util.*;
 
 public class CalcClientEx {
-
     public static void main(String[] args) {
+        BufferedWriter out = null;
+        BufferedReader in = null;
+        Socket socket = null;
+        Scanner sc = new Scanner(System.in);
+        try{
+            socket = new Socket("localhost",9999);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            while(true){
+                System.out.print("계산식 입력(ex. 3 + 8) >>");
+                String outputMsg = sc.nextLine();
+                if(outputMsg.equalsIgnoreCase("bye")){
+                    out.write(outputMsg);
+                    out.flush();
+                    break;
+                }
+                out.write(outputMsg);
+                out.flush();
+                String inputMsg = in.readLine();
+                System.out.println("계산 결과 : "+inputMsg);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                sc.close();
+                if(socket !=null) socket.close();
+            }catch (IOException e){
+                System.out.println("서버와 채팅중 오류");
+            }
+        }
     }
 }
